e0905ad00a2273cc368fa81ff0dedab6d795d54d
Ruslan <ruslan.galikeeff@example.com>
2022-12-15 00:38:31 +0300

    Homework6

---
+++ HomeWork_6/Cat2.java
@@ -0,0 +1,10 @@
+package HomeWork_6;
+
+public class Cat2 {
+    // Структура класса животного
+    public static void main(String[] args) {
+
+        Cat cat1 = new Cat(100, "Вася", "Дворняга", "Рыжий", true, 5, 5);
+
+    }
+}
---
+++ HomeWork_6/Cat3.java
@@ -0,0 +1,11 @@
+package HomeWork_6;
+
+public class Cat3 {
+    public static void main(String[] args) {
+
+        Cat cat1 = new Cat(100, "Вася", "Дворняга", "Рыжий", true, 5, 5);
+
+        System.out.println(cat1.toString(100));
+
+    } 
+}
